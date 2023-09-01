902d8dc661ad3da79e2a8b767f4142abd90f567d
Soma Ando <soma.ando.1227@gmail.com>
2023-09-01 17:51:36 +0900

    予約機能の作成途中

--- practice_bathroom/urls.py
+++ practice_bathroom/urls.py
@@ -20,4 +20,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('loginApp.urls')),
+    path('reserve/', include('reserve.urls'))
 ]
